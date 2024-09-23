7be0bdc51297385e41244806362489f028fe0ac1
ANNISA ALMAGHVIRAH <annisaalmaghvirah332@gmail.com>
2024-09-23 14:18:28 +0800

    Update AngkaSegitiga.py

--- AngkaSegitiga.py
+++ AngkaSegitiga.py
@@ -1,7 +1,4 @@
-n = 6
-print(" " * n + "*")
-for i in range(1, n +1):
-    print(" " * (n - i), end="")
-    for j in range(i):
-        print("*", end=".")
-    print("*")
+for i in range (7,0,-1):
+    for j in range(1,i+1):
+        print(j,end="")
+    print()
